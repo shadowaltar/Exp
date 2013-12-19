f0bb5f6ca3c94cf602e8b5e64f2b672b8b5afa1b
shadowaltar <shadowaltar@gmail.com>
2013-12-19 09:18:14 +0800

    Mortgage related; forgot to commit

---
+++ Exp/InstrumentTypes/Mortgage.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exp.InstrumentTypes
+{
+    public class Mortgage : Security
+    {
+        public Mortgage(int periods, double initialBalance, double annualMortgageRate,
+            int seasoning = 0, double prepaymentMultiplier = 0)
+        {
+            Periods = periods;
+            Seasoning = seasoning;
+            InitialBalance = initialBalance;
+            MortgageStatuses = new List<MortgageStatus>();
+            MortgageRate = annualMortgageRate;
+            if (prepaymentMultiplier != 0)
+            {
+                MortgagePrepayment = new MortgagePrepayment(periods, seasoning, prepaymentMultiplier);
+            }
+        }
+
+        public double InitialBalance { get; protected set; }
+        public int Periods { get; protected set; }
+        public int Seasoning { get; set; }
+        public int ActuralPeriods { get { return Periods - Seasoning; } }
+
+        public double MortgageRate { get; set; }
+        public double MonthlyMortgageRate { get { return MortgageRate / 12; } }
+
+        public virtual List<MortgageStatus> MortgageStatuses { get; protected set; }
+
+        public MortgagePrepayment MortgagePrepayment { get; protected set; }
+
+        public class MortgageStatus
+        {
+            public int Period { get; set; }
+            public double BeginningBalance { get; set; }
+            public double EndingBalance { get; set; }
+
+            public double MortgagePayment { get; set; }
+            public double InterestPayment { get; set; }
+            public double PrincipalRepaid { get; set; }
+
+            public double Prepayment { get; set; }
+
+            public double TotalPrincipalReduced { get { return PrincipalRepaid + Prepayment; } }
+        }
+    }
+
+    public class MortgagePrepayment
+    {
+        public MortgagePrepayment(int periods, int seasoning = 0, double prepaymentMultiplier = 1)
+        {
+            Periods = periods;
+            Seasoning = seasoning;
+            ActuralPeriods = periods - seasoning;
+            PrepaymentMultiplier = prepaymentMultiplier;
+            Cprs = new List<double>();
+            Smms = new List<double>();
+        }
+
+        public int Periods { get; private set; }
+        public int Seasoning { get; private set; }
+        public int ActuralPeriods { get; private set; }
+        public double PrepaymentMultiplier { get; set; }
+        public List<double> Cprs { get; private set; }
+        public List<double> Smms { get; private set; }
+    }
+
+    public class MortgageBackedSecurity : Derivatives
+    {
+        public MortgageBackedSecurity(Mortgage mortgage)
+        {
+            Underlying = mortgage;
+        }
+
+        public Mortgage Mortgage { get { return (Mortgage)Underlying; } }
+        public double PassthroughRate { get; set; }
+        public List<double> ReceivedInterests { get; private set; }
+
+        public double InterestOnlyMbs { get; set; }
+        public double PrincipalOnlyMbs { get; set; }
+    }
+
+    public class InterestOnlyMbs : Derivatives
+    {
+        public InterestOnlyMbs(MortgageBackedSecurity mbs)
+        {
+            Underlying = mbs;
+        }
+    }
+
+    public class PrincipalOnlyMbs : Derivatives
+    {
+        public PrincipalOnlyMbs(MortgageBackedSecurity mbs)
+        {
+            Underlying = mbs;
+        }
+    }
+}
---
+++ Exp/QuantitativeFinance/MbsPricer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exp.InstrumentTypes;
+
+namespace Exp.QuantitativeFinance
+{
+    public class MbsPricer
+    {
+        public static double ComputeCpr(int month, int seasoningMonths = 0, double prepaymentMultiplier = 1)
+        {
+            if (month <= 0)
+                throw new ArgumentException();
+            if (seasoningMonths < 0)
+                throw new ArgumentException();
+            if (prepaymentMultiplier < 0)
+                throw new ArgumentException();
+
+            if (month > 30 - seasoningMonths)
+                return 0.06 * prepaymentMultiplier;
+            return 0.06 * prepaymentMultiplier * (month + seasoningMonths) / 30;
+        }
+
+        public static double ComputeSmm(int month, int seasoningMonths = 0, double prepaymentMultiplier = 1)
+        {
+            if (month <= 0)
+                throw new ArgumentException();
+            if (seasoningMonths < 0)
+                throw new ArgumentException();
+            if (prepaymentMultiplier < 0)
+                throw new ArgumentException();
+
+            return 1 - Math.Pow(1 - ComputeCpr(month, seasoningMonths, prepaymentMultiplier), 1d / 12d);
+        }
+
+        public static double ComputeSmmFromCpr(double cpr)
+        {
+            return 1 - Math.Pow(1 - cpr, 1d / 12d);
+        }
+
+        public static void ComputeMortgage(Mortgage mortgage)
+        {
+            if (mortgage.MortgagePrepayment != null)
+            {
+                ComputePrepayment(mortgage.MortgagePrepayment);
+            }
+
+            var rate = mortgage.MonthlyMortgageRate;
+            var n = mortgage.Periods;
+            var s = mortgage.Seasoning;
+
+            for (int i = 0; i < mortgage.ActuralPeriods; i++)
+            {
+                var currentStatus = new Mortgage.MortgageStatus();
+                mortgage.MortgageStatuses.Add(currentStatus);
+                var balance = i == 0
+                    ? mortgage.InitialBalance : mortgage.MortgageStatuses[i - 1].EndingBalance;
+                currentStatus.BeginningBalance = balance;
+                currentStatus.MortgagePayment = balance * rate / (1 - Math.Pow(1 + rate, -n + s + i));
+                currentStatus.InterestPayment = balance * rate;
+                currentStatus.PrincipalRepaid = currentStatus.MortgagePayment - currentStatus.InterestPayment;
+
+                if (mortgage.MortgagePrepayment != null)
+                {
+                    currentStatus.Prepayment = (balance - currentStatus.PrincipalRepaid)
+                        * mortgage.MortgagePrepayment.Smms[i];
+                }
+                currentStatus.EndingBalance = balance - currentStatus.PrincipalRepaid - currentStatus.Prepayment;
+            }
+        }
+
+        public static void ComputePrepayment(MortgagePrepayment prepayment)
+        {
+            var periods = prepayment.Periods - prepayment.Seasoning;
+            for (int i = 1; i <= periods; i++)
+            {
+                prepayment.Cprs.Add(ComputeCpr(i, prepayment.Seasoning, prepayment.PrepaymentMultiplier));
+                prepayment.Smms.Add(ComputeSmmFromCpr(prepayment.Cprs[i - 1]));
+            }
+        }
+
+        public static void ComputeInterestOnlyMbs(MortgageBackedSecurity mbs)
+        {
+            ComputeMortgage((Mortgage)mbs.Underlying);
+            //
+        }
+    }
+}
